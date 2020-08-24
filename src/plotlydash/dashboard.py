fed9934cd4f580c53615817363298830032d3f65
hadleyrose <hadleyrosemitchell@gmail.com>
2020-08-24 01:14:39 -0500

    Created dropdowns for x, y, and color values of plotly line and scatter plot. Merging back.

--- src/plotlydash/dashboard.py
+++ src/plotlydash/dashboard.py
@@ -42,6 +42,28 @@ plotly_input = dbc.FormGroup(
             ],
             value='line',
             style={'color': 'black'}
+        ),
+        dbc.Label('x-axis'),
+        dcc.Dropdown(
+            id='x_data',
+            options=[dict(label=val, value=val) for val in covid_df.columns],
+            value='date',
+            style={'color': 'black'}
+        ),
+        dbc.Label('y-axis'),
+        dcc.Dropdown(
+            id='y_data',
+            options=[dict(label=val, value=val) for val in covid_df.columns],
+            value='total_cases',
+            style={'color': 'black'}
+        ),
+        dbc.Label('line color'),
+        dcc.Dropdown(
+            id='z_data',
+            options=[dict(label='None', value=None), *(dict(label=val, value=val) for val in covid_df.columns)],
+            # see https://stackoverflow.com/a/50504924 for how to append a list comprehension
+            value='location',
+            style={'color': 'black'}
         )
     ]
 )
@@ -52,6 +74,8 @@ plotly_input = dbc.FormGroup(
 # TODO: option for prophet prediction, prophet graph
 # TODO: option to generate predictions to csv
 # TODO: option to do other ML and select which parts of data to use, warnings based on data type
+# TODO: make prettier
+# TODO: how to handle nans?
 
 dash_form = dbc.Col([
     html.H2('Plotly Dashboard'),
@@ -71,15 +95,19 @@ dash_app.layout = html.Div([
 
 @dash_app.callback(Output('data_plot', 'figure'),
                    [Input('data_dropdown', 'value'),
-                    Input('type_dropdown', 'value')])
-def display_graph(data_dropdown, type_dropdown):
+                    Input('type_dropdown', 'value'),
+                    Input('x_data', 'value'),
+                    Input('y_data', 'value'),
+                    Input('z_data', 'value')])
+def display_graph(data_dropdown, type_dropdown,
+                  x_data, y_data, z_data):
     fig = None
     if data_dropdown != 'covid':  # TODO: update to be user-uploaded data graph
         fig = px.line([{'x': 1, 'y': 2, 'z': 3}])
     else:
         if type_dropdown == 'line':
-            fig = px.line(covid_df, x='date', y='total_cases', color='location')
+            fig = px.line(covid_df, x=x_data, y=y_data, color=z_data)
         elif type_dropdown == 'scatter':
-            fig = px.scatter(covid_df, x='date', y='total_cases', color='location')
+            fig = px.scatter(covid_df, x=x_data, y=y_data, color=z_data)
 
     return fig

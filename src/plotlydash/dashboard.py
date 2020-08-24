1474f93a6396163dc23a74138fdff80391f6e634
hadleyrose <hadleyrosemitchell@gmail.com>
2020-08-24 00:38:06 -0500

    Added plotly type drowpdown, update graph to line or scatter plot, only if covid df.

--- src/plotlydash/dashboard.py
+++ src/plotlydash/dashboard.py
@@ -18,9 +18,9 @@ covid_df = pd.read_csv('./src/static/owid-covid-data.csv')
 
 data_input = dbc.FormGroup(
     [
-        dbc.Label("Select data", html_for="dropdown"),
+        dbc.Label('Select data', html_for='dropdown'),
         dcc.Dropdown(
-            id="data_dropdown",
+            id='data_dropdown',
             options=[
                 {'label': 'COVID-19 Dataset', 'value': 'covid'},
                 {'label': 'Upload your own', 'value': 'upload'}
@@ -31,8 +31,24 @@ data_input = dbc.FormGroup(
     ]
 )
 
+plotly_input = dbc.FormGroup(
+    [
+        dbc.Label('Plot Type'),
+        dcc.Dropdown(
+            id='type_dropdown',
+            options=[
+                {'label': 'line', 'value': 'line'},
+                {'label': 'scatter', 'value': 'scatter'}
+            ],
+            value='line',
+            style={'color': 'black'}
+        )
+    ]
+)
+
 # TODO: preview data option, data preview in modal?
 # TODO: multi select drop down on which columns to include in plotly? one for each feature of plotly? x, y, color, etc.?
+# TODO: add progress bar for all steps that take some time to load (plotly, data upload)
 # TODO: option for prophet prediction, prophet graph
 # TODO: option to generate predictions to csv
 # TODO: option to do other ML and select which parts of data to use, warnings based on data type
@@ -40,7 +56,9 @@ data_input = dbc.FormGroup(
 dash_form = dbc.Col([
     html.H2('Plotly Dashboard'),
     html.H4('Data'),
-    data_input
+    data_input,
+    html.H4('Update Plotly Graph'),
+    plotly_input
 ])
 
 dash_app.layout = html.Div([
@@ -52,11 +70,16 @@ dash_app.layout = html.Div([
 
 
 @dash_app.callback(Output('data_plot', 'figure'),
-                   [Input('data_dropdown', 'value')])
-def display_graph(data_dropdown):
+                   [Input('data_dropdown', 'value'),
+                    Input('type_dropdown', 'value')])
+def display_graph(data_dropdown, type_dropdown):
+    fig = None
     if data_dropdown != 'covid':  # TODO: update to be user-uploaded data graph
         fig = px.line([{'x': 1, 'y': 2, 'z': 3}])
     else:
-        fig = px.line(covid_df, x='date', y='total_cases', color='location')
+        if type_dropdown == 'line':
+            fig = px.line(covid_df, x='date', y='total_cases', color='location')
+        elif type_dropdown == 'scatter':
+            fig = px.scatter(covid_df, x='date', y='total_cases', color='location')
 
     return fig
